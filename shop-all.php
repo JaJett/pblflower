a65acad8c4abf91e059b71ac6c5669fc26535c00
JaJett <riakimuhammad402@gmail.com>
2024-12-23 11:50:34 +0800

    Update 2.1

--- shop-all.php
+++ shop-all.php
@@ -100,7 +100,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="photo-content/logo.JPEG" alt="Sweet Little Garden" height="50" width="50" class="rounded-circle">
+                <img src="photo-content/logo.jpeg" alt="Sweet Little Garden" height="50" width="50" class="rounded-circle">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
