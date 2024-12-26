59779044e64143e18dba4dcc02713f4fe2df348f
JaJett <riakimuhammad402@gmail.com>
2024-12-26 17:22:33 +0800

    update Index

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <style>
         body {
             font-family: 'Arial', sans-serif;
-            background-color: #FCD7E2; /* Warna background pink */
+            background-color: #FFC0D2; /* Warna background pink */
         }
         .navbar {
-            background-color: #00281C !important; /* Warna hijau untuk navbar */
+            background-color: #53806D !important; /* Warna hijau untuk navbar */
         }
         .navbar-toggler-icon {
             color: white;
